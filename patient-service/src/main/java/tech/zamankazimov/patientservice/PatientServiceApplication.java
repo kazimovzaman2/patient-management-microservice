825763ff886eace7071c63f6652210f4572b1fb5
kazimovzaman2 <kazimovzaman2@gmail.com>
2025-04-07 22:13:25 +0400

    Initializer microservice app with patiente service

---
+++ patient-service/src/main/java/tech/zamankazimov/patientservice/PatientServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.zamankazimov.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+
+}
---
+++ patient-service/src/test/java/tech/zamankazimov/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.zamankazimov.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
