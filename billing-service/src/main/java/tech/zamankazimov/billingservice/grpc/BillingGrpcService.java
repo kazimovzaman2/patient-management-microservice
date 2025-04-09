2792c32dd7b619d475e308b80f55ae2dfa230a7c
kazimovzaman2 <kazimovzaman2@gmail.com>
2025-04-09 21:34:57 +0400

    billing-service initialized. Simple service without any business logic (it just use dummy data).

---
+++ billing-service/src/main/java/tech/zamankazimov/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,30 @@
+package tech.zamankazimov.billingservice.grpc;
+
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import billing.BillingRequest;
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received: {}", billingRequest.toString());
+
+        // Business logic to create a billing account would go here
+
+        BillingResponse response = BillingResponse.newBuilder().setAccountId("12345").setStatus("ACTIVE").build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
+
+}
