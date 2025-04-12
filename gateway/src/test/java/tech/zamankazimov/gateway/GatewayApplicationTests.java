2de9f0655da6e90ddc889970274038072dde3ef3
kazimovzaman2 <kazimovzaman2@gmail.com>
2025-04-12 20:29:12 +0400

    Add gateway module with initial configuration and Docker setup

--- gateway/src/test/java/tech/zamankazimov/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/tech/zamankazimov/gateway/GatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package tech.zamankazimov.apigateway;
+package tech.zamankazimov.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class GatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
